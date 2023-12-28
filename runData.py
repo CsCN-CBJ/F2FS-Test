6f3192cba30f1cef10d1392c173c6694ae0078ce
陈柏居 <200110705@stu.hit.edu.cn>
2023-12-28 13:58:01 +0800

    runFioAll, replace prev data

--- runData.py
+++ runData.py
@@ -26,7 +26,8 @@ def matchAmplification(filename: str):
     with open(filename, "r") as f:
         content = f.read()
         wCnt = matchFirstInt(r"total_write_count (\d+)", content)
-        assert wCnt == TOTAL_WRTIE >> 12
+        if wCnt != TOTAL_WRTIE >> 12:
+            print(f"total_write_count: {wCnt}, filename: {filename}")
         wDedupCnt = matchFirstInt(r"total_dedup_count (\d+)", content)
         wMetaCnt = matchFirstInt(r"change_to_disk_count (\d+)", content)
 
@@ -35,6 +36,7 @@ def matchAmplification(filename: str):
             wRefCnt = matchFirstInt(r"total_num_enter_write_ref_file (\d+)", content)
             wMetaAll = matchFirstInt(r"total_num_enter_write_metadata_func (\d+)", content)
             assert wMetaAll == wMetaCnt + wRefCnt
+            wMetaAll = matchFirstInt(r"change_to_disk_count (\d+)", content)
         except KeyError:
             # 识别DysDedup的amplification
             wRefCnt = matchFirstInt(r"global_ref_write_count (\d+)", content)
@@ -50,7 +52,11 @@ def runTraceData():
     for trace in traceList:
         for fs in fsList:
             os.system(f"runData.bat 10000 {path}{trace} {fs}")
-            os.rename(f"./data/result.txt", f"./data/{fs}_{trace.split('.')[0]}.txt")
+
+            dstName = f"./data/{fs}_{trace.split('.')[0]}.txt"
+            if os.path.exists(dstName):
+                os.remove(dstName)
+            os.rename(f"./data/result.txt", dstName)
     return
 
 
@@ -64,6 +70,10 @@ def runFioData():
                 lruLen = (TOTAL_WRTIE >> 12) * (100 - dupRatio) / 100 * lruRatio / 100
                 lruLen = int(lruLen)
                 os.system(f"runData.bat {lruLen} {dupRatio} {fs}")
+
+                dstName = f"./data/{fs}_{dupRatio}_{lruRatio}.txt"
+                if os.path.exists(dstName):
+                    os.remove(dstName)
                 os.rename(f"./data/result.txt", f"./data/{fs}_{dupRatio}_{lruRatio}.txt")
 
 
@@ -96,6 +106,39 @@ def drawFioDup():
     # 显示图例
     plt.legend()
     # 显示图形
+    plt.show()
+
+
+def drawFioAll():
+    dupRatios = [0, 25, 50, 75]
+    lruRatios = [3, 5, 10, 20, 50, 100]
+    bar_width = 0.35
+    opacity = 0.8
+    colors = ['b', 'r', 'g', 'y', 'c', 'm']
+    xRange = range(len(lruRatios))
+
+    plt.figure(figsize=(10, 10), dpi=100)
+    for dupRatio in dupRatios:
+        plt.subplot(2, 2, dupRatio // 25 + 1)
+        for index, fs in enumerate(fsList):
+            dataList = []
+            for lruRatio in lruRatios:
+                amp = matchAmplification(f"./data/{fs}_{dupRatio}_{lruRatio}.txt")
+                dataList.append(amp)
+            plt.bar([i + bar_width * index for i in xRange],
+                    dataList, bar_width, color=colors[index], alpha=opacity, label=fs)
+
+        # 设置横纵坐标的标签和标题
+        plt.xlabel('lruRatios')
+        plt.ylabel('Amplification')
+        # plt.title('Comparison of DedupFS and SmartDedup')
+        plt.title(f"dupRatio: {dupRatio}%")
+        plt.xticks([i + bar_width / 2 for i in xRange], lruRatios)
+
+    # 显示图例
+    plt.legend()
+    # 显示图形
+    plt.savefig("./data/FioAll.png")
     plt.show()
 
 
@@ -136,7 +179,8 @@ def drawTrace():
 if __name__ == "__main__":
     # draw()
     # runTraceData()
-    runFioData()
+    # runFioData()
+    drawFioAll()
     # drawFioDup()
     # print(matchAmplification("./data/DedupFS_hitsztrace_4GB.txt"))
     # print(matchAmplification("./data/smartdedup_hitsztrace_4GB.txt"))
